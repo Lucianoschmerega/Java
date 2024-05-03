bebc8f056b9a9f70967cbc9acd74a4f144c8845d
Luciano <Luciano.schmerega.filho@escola.pr.gov.br>
2024-05-03 12:34:54 -0300

    aula de programação

---
+++ Tipe-Number.js
@@ -0,0 +1,10 @@
+// tipo number 
+
+// declaração variação number
+
+const num1 = 10;
+const num2 = 10;
+
+const soma = num1 + num2;
+
+console.log(soma);
--- TypeString.js
+++ TypeString.js
@@ -1,1 +1,8 @@
-const name = "Hello world";
+const string1 = "jose";
+const string2 = "Valim";
+
+// sempre que for escrever algo em uma const tem q colocar entre "aspas duplas"
+
+const nome = string1 + string2;
+
+console.log(nome);
