ab31f91723fe0cb4ea83f043efe8aa27328ace71
Luciano <Luciano.schmerega.filho@escola.pr.gov.br>
2024-05-03 09:30:32 -0300

    Trabalho

---
+++ meu.js
@@ -0,0 +1,22 @@
+//number
+
+//variavel endereço e espaço
+//armazenamento em memoria
+
+ const MyNunber =3;
+
+ const PrimeiroNumero = 3;
+
+ const SegundoNumero = 2;
+
+//proibido variaveis de mesmo nome
+//const my number = 5
+
+
+//soma de duas variaveis
+
+ const soma = PrimeiroNumero + SegundoNumero;
+
+//comando de autipute ou comando de mostrar na tela 
+
+ console.log(soma);
