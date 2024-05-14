111230821e233248f978c5beb71ccc605b6066b8
Luck <Luciano.schmerega.filho@escola.pr.gov.br>
2024-05-14 09:28:42 -0300

    Js do professor

--- TypeString.js
+++ TypeString.js
@@ -1,8 +1,29 @@
-const string1 = "jose";
+// Tipo String
+// Cadeia de caracteres (simbolos)
+// Exemplo:
+// Frases
+// Textos ...
+
+const string1 = "Jose";
 const string2 = "Valim";
 
-// sempre que for escrever algo em uma const tem q colocar entre "aspas duplas"
-
-const nome = string1 + string2;
+// Concatenar
+// Juntar 2 strings
+const nome = string1 + " " + string2;
 
 console.log(nome);
+
+// Exemplo
+const nome2 = "Jose";
+const sobrenome = "Valim";
+const idade = 30;
+
+console.log("Nome: " + nome2);
+console.log("Sobrenome: " + sobrenome);
+console.log("Idade: " + idade);
+
+
+
+
+
+
