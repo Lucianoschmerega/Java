13879e71aab250901bbf2f07c6379cbe3bfdefdb
Luck <Luciano.schmerega.filho@escola.pr.gov.br>
2024-05-17 09:57:06 -0300

    AULA

---
+++ trueorfalse.js
@@ -0,0 +1,18 @@
+console.log(0 == false)
+console.log(1 == true)
+console.log(1 == false)
+console.log(0 == true)
+
+console.log("" == false)
+
+let minhaVar;
+let testnull;
+
+console.log(minhaVar)
+console.log(testnull)
+
+let number = 3;
+let testo = "Alura";
+
+console.log(typeof(minhaVar))
+console.log(typeof(testnull))
